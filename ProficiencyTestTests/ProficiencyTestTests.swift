03906db2bc8ec954d0e4e68332d637ed396d5b4e
Vivek <Vivek>
2019-05-29 11:08:05 +0530

    Initial Commit

---
+++ ProficiencyTestTests/ProficiencyTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProficiencyTestTests.swift
+//  ProficiencyTestTests
+//
+//  Created by mac_admin on 29/05/19.
+//  Copyright © 2019 Infosys. All rights reserved.
+//
+
+import XCTest
+@testable import ProficiencyTest
+
+class ProficiencyTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
